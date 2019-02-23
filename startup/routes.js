0a15cb7b45ffceb555f076a0ed31c595491665e1
Dillon <dillon.embreus@gmail.com>
2019-02-23 14:27:36 -0700

    modified routes

--- startup/routes.js
+++ startup/routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const genres = require("../routes/genres");
 const customers = require("../routes/customers");
 const movies = require("../routes/movies");
@@ -10,6 +11,7 @@ const error = require("../middleware/error");
 
 module.exports = function(app) {
   app.use(express.json());
+  app.use(cors());
   app.use("/api/genres", genres);
   app.use("/api/customers", customers);
   app.use("/api/movies", movies);
